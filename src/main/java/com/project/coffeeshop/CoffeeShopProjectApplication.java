50bb0e96eb7abd9a4e318f14f2e79013040d29b0
Richmond <130723287+richmondpangan@users.noreply.github.com>
2023-11-27 22:15:25 +0800

    Initial commit

---
+++ src/main/java/com/project/coffeeshop/CoffeeShopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/coffeeshop/CoffeeShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
