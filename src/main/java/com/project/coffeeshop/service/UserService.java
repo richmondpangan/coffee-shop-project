1e4d11b2d66fa51f3b2a150c3a78348c20451237
Richmond <130723287+richmondpangan@users.noreply.github.com>
2023-11-28 18:18:43 +0800

    Added Some Functionalities:
    - Get user by ID
    - Register new user
    - Update existing user
    - Delete an existing user

--- src/main/java/com/project/coffeeshop/service/UserService.java
+++ src/main/java/com/project/coffeeshop/service/UserService.java
@@ -4,10 +4,12 @@
  */
 package com.project.coffeeshop.service;
 
+import com.project.coffeeshop.exception.UserNotFoundException;
 import com.project.coffeeshop.model.User;
 import com.project.coffeeshop.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +28,31 @@ public class UserService {
     public User getUserById(Integer userId) {
         return userRepository.findById(userId).get();
     }
+    
+    public void registerUser(User user) {
+        userRepository.save(user);
+    }
+    
+    public void updateUser(Integer userId, User user) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            existingUser.setUserFirstName(user.getUserFirstName());
+            existingUser.setUserLastName(user.getUserLastName());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPassword(user.getPassword());
+            existingUser.setRole(user.getRole());
+            
+            userRepository.save(existingUser);
+        }
+        else {
+            throw new UserNotFoundException("User with ID " + userId + " not found");
+        }
+    }
+    
+    public void deleteUserById(Integer userId) {
+        userRepository.deleteById(userId);
+    }
+    
+    //Login
 }
